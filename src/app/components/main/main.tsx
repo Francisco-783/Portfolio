2f7dcf52ec001d15ccd1bf4c8b8e72b10acaea10
Francisco_I <francisco.insaurralde21@gmail.com>
2023-07-27 21:34:58 -0300

    main echo y navbar solucionada

--- src/app/components/main/main.tsx
+++ src/app/components/main/main.tsx
@@ -10,19 +10,22 @@ const Main: React.FC = () => {
 
 
   return (
-    <section className={style.container}>
-      <h1>FullStack Web Developer</h1>
-      <label>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa totam deleniti deserunt, repellendus saepe ab qui, labore dignissimos unde omnis alias quam maxime eligendi pariatur eaque rerum dolores modi iure?</label>
-      <Link href="https://www.linkedin.com/in/francisco-insaurralde-539109220/">
-      <Image src="/assets/linkedinLogo.png" width={50} height={50} alt="linkedin logo" />
-      </Link>
-      <Link href="https://github.com/Francisco-783">
-      <Image src="/assets/githublogo.png" width={50} height={50} alt="github logo" />
-      </Link>
-      <Link href="https://github.com/Francisco-783">
-      <label>MI CV</label>
-      </Link>
-      <Image src="/assets/Foto-blanco.png" width={400} height={400} alt="developer's photo" />
+    <section className={style.main}>
+      <div>
+        <p className={style.subtitle}>Hola, soy Francisco Insaurralde.</p>
+        <h1>Full Stack Web </h1>
+        <h1>Developer</h1>
+        <Link href="https://www.linkedin.com/in/francisco-insaurralde-539109220/">
+          <Image src="/assets/s.png" width={40} height={40} alt="linkedin logo" />
+        </Link>
+        <Link href="https://github.com/Francisco-783">
+          <Image src="/assets/githublogo.png" width={40} height={40} alt="github logo" />
+        </Link>
+        <Link href="https://github.com/Francisco-783">
+          <Image src="/assets/pdf.png" width={40} height={40} alt="github logo" />
+        </Link>
+      </div>
+      <Image src="/assets/main.gif" width={450} height={400} alt="developer's photo" className={style.photo}/>
     </section>
   );
 };
