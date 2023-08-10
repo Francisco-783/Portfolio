b169bbe84ce451af923234c06fdb7d3df25c64fa
Francisco_I <francisco.insaurralde21@gmail.com>
2023-08-10 20:15:12 -0300

    navbar terminada; solo falta agregar cv

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
+"use client"
+
+import {useState} from 'react';
 import style from './navbar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
+    const [navbar, setNavbar] = useState(false);
+    
+    const SwichMenu = () =>{
+        setNavbar(!navbar)
+        console.log(navbar)
+    }
   return (
       <div className={style.navbar}>
-          <h2>
-              <a href='#start'>Francisco.dev</a>
-          </h2>
-         <ul>
-          <li>
-              <a href='#start'>Inicio</a>
-          </li>
-          <li>
-              <a href='#about'>Sobre Mi</a>
-          </li>   
-          <li>
-              <a href='#skills'>Proyectos</a>
-          </li>
-          <li>
-              <a href='#contact'>Cotacto</a>
-          </li>
-          <li>Curriculum Vitae</li>
+        <h2>
+            <a href='#start'>Francisco.dev</a>
+        </h2>
+        <FontAwesomeIcon icon={faBars} className={`${style.menuButton} ${!navbar ? style.menuBlue : ''}`} onClick={SwichMenu}/>
+        <ul className={navbar ? style.closed : ""}>
+            <li onClick={SwichMenu}>
+                <a href='#start'>Inicio</a>
+            </li>
+            <li onClick={SwichMenu}>
+                <a href='#about'>Sobre Mi</a>
+            </li>   
+            <li onClick={SwichMenu}>
+                <a href='#skills'>Proyectos</a>
+            </li>
+            <li onClick={SwichMenu}>
+                <a href='#contact'>Cotacto</a>
+            </li>
+          <li onClick={SwichMenu}><a href='#contact' className={style.cvLink}>Curriculum Vitae</a></li>
          </ul>
+         
       </div>
   )
 }
--- src/app/components/skills/skills.tsx
+++ src/app/components/skills/skills.tsx
@@ -6,20 +6,20 @@ import Image from 'next/image';
 const Skills = () => {
     return (
         <section className={style.skills} id="skills">
-            <div className={style.skillsContainer}>
+            <div className={`${style.skillsContainer} ${style.headSkillsContainer}`}>
                 <Image src="/assets/Tech/front/reactLogo.png" width={55} height={50} alt="react logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/front/reduxLogo.png" width={50} height={50} alt="redux logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/front/sassLogo.png" width={60} height={50} alt="sass logo" className={style.skillPhoto}/>
             </div>
-            <div className={style.skillsContainer}>
-                <Image src="/assets/Tech/back/mongodbLogo.png" width={30} height={50} alt="mongodb logo"  className={style.skillPhoto}/>
+            <div className={`${style.skillsContainer} ${style.midSkillsContainer}`}>
+                <Image src="/assets/Tech/back/mongodbLogo.png" width={30} height={50} alt="mongodb logo"   className={`${style.skillPhoto} ${style.mongoIcon}`}/>
                 <Image src="/assets/Tech/back/expressJSLogo.png" width={50} height={50} alt="expressJS logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/back/nodeJSLogo.png" width={45} height={50} alt="nodeJS logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/back/postgreeSQLLogo.png" width={50} height={50} alt="postgreeSQL logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/back/sequelizeLogo.png" width={50} height={50} alt="sequelize logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/back/NestJS.png" width={50} height={50} alt="sequelize logo" className={style.skillPhoto}/>
             </div>
-            <div className={style.skillsContainer}>
+            <div className={`${style.skillsContainer} ${style.headSkillsContainer}`}>
                 <Image src="/assets/Tech/gitlog.png" width={50} height={50} alt="git logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/js.png" width={50} height={50} alt="js logo" className={style.skillPhoto}/>
                 <Image src="/assets/Tech/typescriptLog.png" width={50} height={50} alt="typescript logo" className={style.skillPhoto}/>
